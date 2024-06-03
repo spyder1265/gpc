07f9ae2f7699641db2b87530795af612ad25899d
Titus Likhi <60445905+spyder1265@users.noreply.github.com>
2024-06-03 16:50:46 +0000

    chore: Update import paths for Navbar and Footer components in about and other pages

--- app/about/page.js
+++ app/about/page.js
@@ -21,7 +21,7 @@ export default function About() {
           </div>
           <div className='md:py-24 py-12 px-6 md:px-16 w-full'>
             <div className='flex flex-col gap-y-1 items-start justify-center text-left h-full'>
-              <h1 className='text-3xl md:text-5xl font-bold'>About</h1>
+              <h1 className='text-3xl md:text-5xl font-bold'>about</h1>
             </div>
           </div>
         </div>
@@ -31,21 +31,21 @@ export default function About() {
         <div className='flex h-14 w-full bg-yellowShade/80 justify-center overflow-hidden relative'>
           <div className='flex text-white font-semibold'>
             <div className='hover:bg-yellowShade py-4 px-2 active:bg-yellowActive'>
-              <a href='/about'>About Us</a>
+              <a href='/about'>about us</a>
             </div>
             <div className='hover:bg-yellowShade h-full flex items-center  active:bg-yellowActive'>
               <a href='/about/our-team' className='py-4 h-full px-2'>
-                Our Team
+                our team
               </a>
             </div>
             <div className='hover:bg-yellowShade h-full flex items-center  active:bg-yellowActive'>
               <a href='/about/im-new' className='py-4 h-full px-2'>
-                I'm New
+                i'm new
               </a>
             </div>
             <div className='hover:bg-yellowShade h-full flex items-center active:bg-yellowActive'>
               <a href='/about/our-beliefs' className='py-4 h-full px-2'>
-                Our Beliefs
+                our beliefs
               </a>
             </div>
           </div>
@@ -56,84 +56,75 @@ export default function About() {
         <div className='flex flex-col h-full basis-4/6'>
           <div className='flex py-4 justify-center text-center md:pt-24 space-y-1 text-yellowShade'>
             <p className='flex justify-center font-bold text-4xl leading-[46.8px] mb-4 tracking-normal'>
-              Welcome to Grace Pavilion Chapel!
+              welcome to grace pavilion chapel!
             </p>
           </div>
           <div className='flex flex-col text-left mx-10 md:mx-80 gap-5'>
             <h1 className='flex justify-center text-center space-y-1 text-yellowShade md:text-[20px] font-bold'>
-              Who We Are
+              who we are
             </h1>
             <p className='dark:text-neutral-300'>
-              We're thrilled that you've discovered us! At Grace Pavilion
-              Chapel, we are more than just a congregation; we are a vibrant
-              community of followers of Jesus Christ. Rooted in the belief that
-              the essence of the Kingdom lies in fostering genuine
-              relationships, we prioritize our connection with God and with one
-              another (Matthew 22:36-40).
+              we're thrilled that you've discovered us! at grace pavilion
+              chapel, we are more than just a congregation; we are a vibrant
+              community of followers of jesus christ. rooted in the belief that
+              the essence of the kingdom lies in fostering genuine
+              relationships, we prioritize our connection with god and with one
+              another (matthew 22:36-40).
             </p>
 
             <h1 className='flex justify-center text-center space-y-1 text-yellowShade md:text-[20px] font-bold'>
-              Our Vision
+              our vision
             </h1>
             <p className='dark:text-neutral-300'>
-              Our vision is simple yet profound: to embody and extend the love
-              of Christ in all that we do. We strive to create an atmosphere
-              where everyone feels welcomed, accepted, and loved—where grace
-              abounds and lives are transformed. Whether you're exploring faith
-              for the first time, seeking a place to grow spiritually, or
-              longing for authentic community, you'll find a home here at Grace
-              Pavilion Chapel.
+              vision statement bringing the blind to the knowledge of christ. (2
+              corinthians 4:4)
             </p>
 
             <h1 className='flex justify-center text-center space-y-1 text-yellowShade md:text-[20px] font-bold'>
-              Our Mission
+              our mission
             </h1>
 
             <p className='dark:text-neutral-300'>
-              Our mission is to love God passionately, love others
-              unconditionally, and make disciples intentionally. We are
-              committed to living out the teachings of Jesus Christ and sharing
-              His message of hope, redemption, and reconciliation with the
-              world. Through worship, fellowship, service, and outreach, we seek
-              to make a positive impact in our local community and beyond.
+              pulling down strong holds, taking territories for christ through
+              the power of his word (the sword of the spirit)
             </p>
 
             <h1 className='flex justify-center text-center space-y-1 text-yellowShade md:text-[20px] font-bold'>
-              What to Expect
+              what to expect
             </h1>
 
             <p className='dark:text-neutral-300'>
-              When you walk through our doors, you can expect to encounter a
+              when you walk through our doors, you can expect to encounter a
               warm and welcoming atmosphere, heartfelt worship, and practical,
-              relevant teaching from the Bible. Whether you're joining us for
-              one of our Sunday services, participating in a small group, or
+              relevant teaching from the bible. whether you're joining us for
+              one of our sunday services, participating in a small group, or
               attending a special event, you'll find a community that is eager
               to walk alongside you on your journey of faith.
             </p>
 
             <h1 className='flex justify-center text-center space-y-1 text-yellowShade md:text-[20px] font-bold'>
-              Get Involved
+              get involved
             </h1>
 
             <p className='dark:text-neutral-300'>
-              There are numerous opportunities for you to get involved and
-              become an active part of our church family. From serving on a
-              ministry team to joining a Bible study group, there's something
+              there are numerous opportunities for you to get involved and
+              become an active part of our church family. from serving on a
+              ministry team to joining a bible study group, there's something
               for everyone to contribute their gifts and talents to the work of
-              God's Kingdom. We believe that each person has a unique role to
-              play in advancing the cause of Christ, and we invite you to
+              god's kingdom. we believe that each person has a unique role to
+              play in advancing the cause of christ, and we invite you to
               discover yours.
             </p>
 
             <h1 className='flex justify-center text-center space-y-1 text-yellowShade md:text-[20px] font-bold'>
-              Contact Us
+              contact us
             </h1>
 
             <p className='dark:text-neutral-300'>
-              If you have any questions or would like more information about
-              Grace Pavilion Chapel, please don't hesitate to reach out to us.
-              Our friendly staff and volunteers are here to assist you and help
-              you feel at home. We look forward to connecting with you and
+              if you have any questions or would like more information about
+              grace pavilion chapel, please don't hesitate to reach out to us.
+              our friendly staff and volunteers are here to assist you and help
+              you feel at home. we look forward to connecting with you and
               journeying together in faith, hope, and love.
             </p>
           </div>
