7287d33f2e854e8979ec3d2a55fe951d049b3b97
Ignatius Nyendah <ignyendah@gmail.com>
2025-03-06 12:45:58 +0000

    Updates

--- app/about/our-team/page.js
+++ app/about/our-team/page.js
@@ -172,7 +172,7 @@ export default function OurTeam() {
             <div className='text-center flex flex-col items-center text-gray-500'>
               <img
                 className='mx-auto mb-4 w-36 h-36 object-cover rounded-full'
-                src='/img/Newton.jpeg'
+                src='/img/bg1.jpg'
                 alt='Helene Avatar'
               />
               <h3 className='mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-neutral-50'>
@@ -261,7 +261,7 @@ export default function OurTeam() {
             <div className='text-center flex flex-col items-center text-gray-500'>
               <img
                 className='mx-auto mb-4 w-36 h-36 object-cover rounded-full'
-                src='/img/Ameyaw.jpg'
+                src='/img/bg1.jpg'
                 alt='Helene Avatar'
               />
               <h3 className='mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-neutral-50'>
